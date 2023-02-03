6b034ed6bb9befbb76eacdc56caf145f6d5144fd
HarshitMahajan <harma2003@gmail.com>
2023-02-03 12:29:32 +0530

    Upadted variables, Boolean and Strings

---
+++ JavaScript/day1.js
@@ -0,0 +1,12 @@
+// It follows LCC( Lower camel Case)
+
+
+// Basic Syntax of JavaScript
+
+ var fruit ="Apple";  // Global Variable
+
+function Myfunction(){              // Creation of new function
+    var num = 45;                   // Created local Variable 
+    console.log(fruit);             // Value of Global Variable
+    console.log("\n" + num);        // Value of Local Variable
+}
