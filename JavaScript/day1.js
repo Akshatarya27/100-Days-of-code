fe44ff8bde02a3a837ab19965e3a8c97e3416109
HarshitMahajan <harma2003@gmail.com>
2023-02-03 12:41:50 +0530

    Update

--- JavaScript/day1.js
+++ JavaScript/day1.js
@@ -10,3 +10,19 @@ function Myfunction(){              // Creation of new function
     console.log(fruit);             // Value of Global Variable
     console.log("\n" + num);        // Value of Local Variable
 }
+
+// Boolean Algebra 
+
+const LoggedItem = true; // we can use either true or false
+
+// Strings 
+//First way to decalre a string 
+const af = "Hello";
+
+// Second way 
+const s = 'Hello Guys';
+
+// We can also declare strings using variables. For example
+ const newMessage = `Yo ${af}, Greetings${s}`;
+
+ 
