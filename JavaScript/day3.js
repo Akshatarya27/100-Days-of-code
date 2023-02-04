cc5cb74243a71051a0132d80bc07e35bfe6e49cb
HarshitMahajan <harma2003@gmail.com>
2023-02-04 15:55:38 +0530

    Conditional statements

--- JavaScript/day3.js
+++ JavaScript/day3.js
@@ -1,12 +1,15 @@
 //conditional statements
 
-//If and Else-
+//If and Else and else if -
 
 //Basic Syntax
 let a = 23;
 let b = 34;
 if (a == b){
     console.log("Helloooo")
+}
+else if(a >b){
+    return yeahhhh;
 }
 else{
     return nooooo;
