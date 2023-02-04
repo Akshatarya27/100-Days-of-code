bc8aea0f0cae24864db1f868526213d8df08f37f
HarshitMahajan <harma2003@gmail.com>
2023-02-04 09:24:03 +0530

    Day 2

---
+++ JavaScript/day2.js
@@ -0,0 +1,20 @@
+// Functions - these are re-usable code
+
+function getMessage(){          //Functon Creation
+    return HEllo;
+}
+
+const v = getMessage();         //Function Calling
+//Output will be- HEllo
+
+
+// Adding Two Variables
+
+add throw(2);                   // Defined Number
+function TwoSum(input){             
+    const output = input+2;     //Function to add
+    return output;
+
+}
+
+
